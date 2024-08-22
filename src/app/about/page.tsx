038b77f1a5640724047b253e0a4b2428f27ae18b
tkppop <ptk16@case.edu>
2024-08-21 22:19:30 -0400

    adding about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div> 
+          <h1> This is a about page</h1>
+          <p> More content to follow</p>
+        </div>
+      </div>
+    );
+  }
